d522afa6bdb00904aee18bd4e9ac7ef1e14f16eb
toukir15 <toukir.developer.bd@gmail.com>
2024-05-22 23:33:07 +0600

    d

--- README.md
+++
@@ -1,5 +0,0 @@
-## Locally run instruction
-
-1. At first clone the git repository using command "git clone [repo name]"
-2. Then install node modules using command "npm install"
-3. Finally, run the project locally using command "npm run start:dev"
--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,8 @@ import { studentRoutes } from "./app/Modules/Product/product.route";
 import { orderRoutes } from "./app/Modules/Order/order.route";
 import { notFoundHandler } from "./app/Middlewares/notFoundRoute";
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "server is running" });
 });
