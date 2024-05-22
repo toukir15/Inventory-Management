4be4d5e195e9217916eae2cd1749af72e09f4fc6
toukir15 <toukir.developer.bd@gmail.com>
2024-05-22 19:47:09 +0600

    updated code

--- src/Middlewares/startPoint.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export const startPoint = (req: Request, res: Response) => {
-  res.status(200).json({
-    success: true,
-    message: "API starting point",
-  });
-};
--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,9 @@ import cors from "cors";
 import { studentRoutes } from "./Modules/Product/product.route";
 import { orderRoutes } from "./Modules/Order/order.route";
 import { notFoundHandler } from "./Middlewares/notFoundRoute";
-import { startPoint } from "./Middlewares/startPoint";
 
 app.use(express.json());
 app.use(cors());
 app.use("/api", studentRoutes);
 app.use("/api", orderRoutes);
-app.use(startPoint);
 app.use(notFoundHandler);
